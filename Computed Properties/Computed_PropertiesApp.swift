f5e33aa6e13116012ce0a7a60e2edf705ca2b5f6
Wongi <hpurs95@gmail.com>
2022-07-06 20:54:13 +0900

    Initial Commit

---
+++ Computed Properties/Computed_PropertiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Computed_PropertiesApp.swift
+//  Computed Properties
+//
+//  Created by MacBook Air on 2022/07/06.
+//
+
+import SwiftUI
+
+@main
+struct Computed_PropertiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
